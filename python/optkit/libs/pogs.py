a5d94f3c152cc1a2bbeddc44688fca165fb2de74
bungun <ungun@stanford.edu>
2016-01-10 16:13:26 -0800

    correction to support feasible dual

--- python/optkit/libs/pogs.py
+++ python/optkit/libs/pogs.py
@@ -101,7 +101,9 @@ class PogsLibs(object):
 				_fields_ = [('x', ok_float_p),
 							('y', ok_float_p),
 							('mu', ok_float_p),
-							('nu', ok_float_p)]		
+							('nu', ok_float_p),
+							('mu1', ok_float_p),
+							('nu1', ok_float_p)]		
 
 			pogs_output_p = POINTER(PogsOutput)			
 			lib.pogs_output = PogsOutput
