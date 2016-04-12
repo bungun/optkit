e9ccd097a76410cddd32c647814ed96b7175c6de
bungun <ungun@stanford.edu>
2016-04-12 08:55:51 -0700

    building clustering.h

--- include/optkit_clustering.h
+++ include/optkit_clustering.h
@@ -8,24 +8,31 @@ extern "C" {
 #endif
 
 typedef struct cluster_aid {
-	void * hdl; /* linalg handle */
-	vector a, c, d, c_squared_full, c_squared, c_tentative;
-        matrix D_full, D;
-        size_t reassigned, idx_tentative;
-        ok_float maxdist;
-        int freeable;
+	void * hdl, /* linalg handle */
+	vector a, c, d, c_squared_full, c_squared, c_tentative,
+        matrix D_full, D,
+        size_t reassigned, idx_tentative,
+        ok_float maxdist,
+        int freeable
 } cluster_aid;
 
 typedef struct upsamplingvec {
-	size_t * vals;
-	size_t size, shift;
-}
+	size_t * vals,
+	size_t size, shift
+};
 
-#ifdef __cplusplus
-}
-#endif
+void upsamplingvec_alloc(upsamplingvec * u, size_t size);
+void upsamplingvec_calloc(upsamplingvec * u, size_t size);
+void upsamplingvec_free(upsamplingvec * u);
+void upsamplingvec_subvector(upsamplingvec * usub, upsamplingvec * u,
+	size_t offset, size_t size, size_t shift);
 
-#endif /* OPTKIT_CLUSTERING_H_ */
+void cluster_aid_init(matrix * A, matrix * C);
+void cluster_aid_finish(cluster_aid * h);
+void cluster(matrix * A, matrix * C, upsamplingvec * a2c, cluster_aid ** helper,
+	ok_float maxdist);
+void calculate_centroids(matrix * A, matrix * C, upsamplingvec * a2c,
+	vector * counts, cluster_aid * h);
 
 void k_means(matrix * A, matrix * C, upsamplingvec * a2c, vector * counts,
 	cluster_aid * h, const ok_float dist_reltol, const int change_tol,
@@ -36,3 +43,10 @@ void blockwise_kmeans(matrix * A, matrix * C, upsamplingvec * a2c,
 	const size_t * blocksC, const size_t maxiter, const ok_float dist_tol,
 	const ok_float * change_tols, const uint verbose);
 
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* OPTKIT_CLUSTERING_H_ */
