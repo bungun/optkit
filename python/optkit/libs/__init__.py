7bef4ee453ff6d4dc341db134339cbd7458e5d3e
bungun <ungun@stanford.edu>
2016-05-03 14:38:41 -0700

    (lib wrappers) eliminate default import of libraries to reduce module interdependence

--- python/optkit/libs/__init__.py
+++
@@ -1,7 +0,0 @@
-from optkit.libs.linsys import DenseLinsysLibs, SparseLinsysLibs
-from optkit.libs.equilibration import EquilibrationLibs
-from optkit.libs.projector import ProjectorLibs
-from optkit.libs.prox import ProxLibs
-from optkit.libs.pogs import PogsLibs, PogsAbstractLibs
-from optkit.libs.operator import OperatorLibs
-# from optkit.libs.cg import ConjugateGradientLibs
