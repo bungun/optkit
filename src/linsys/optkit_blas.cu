cd4f1f9ca0a000a2981e1ff10796862e156f9905
bungun <ungun@stanford.edu>
2016-05-04 16:55:52 -0700

    compile blas.cu after error checking changes

--- src/linsys/optkit_blas.cu
+++ src/linsys/optkit_blas.cu
@@ -5,20 +5,9 @@
 extern "C" {
 #endif
 
-static int __matrix_order_compat(const matrix * A, const matrix * B,
-	const char * nm_A, const char * nm_B, const char * nm_routine)
-{
-	if (A->order == B->order)
-		return 1;
-
-	printf("OPTKIT ERROR (%s) matrices %s and %s must have same layout.\n",
-		nm_routine, nm_A, nm_B);
-	return 0;
-}
-
 ok_status blas_make_handle(void ** handle)
 {
-	cublasStatus_t status;
+	ok_status err = OPTKIT_SUCCESS;
 	cublasHandle_t * hdl;
 	hdl = (cublasHandle_t *) malloc(sizeof(cublasHandle_t));
 	err = OK_SCAN_CUBLAS( cublasCreate(hdl) );
@@ -35,10 +24,11 @@ ok_status blas_make_handle(void ** handle)
 
 ok_status blas_destroy_handle(void * handle)
 {
-	cublasDestroy(*(cublasHandle_t *) handle);
-	return OK_STATUS_CUDA;
+	OK_CHECK_PTR(handle);
+	ok_status err = OK_SCAN_CUBLAS(
+		cublasDestroy(*(cublasHandle_t *) handle) );
 	ok_free(handle);
-	return OPTKIT_SUCCESS;
+	return err;
 }
 
 /* BLAS LEVEL 1 */
@@ -46,8 +36,7 @@ ok_status blas_axpy(void * linalg_handle, ok_float alpha, const vector * x,
 	vector * y)
 {
 	ok_status err = OPTKIT_SUCCESS;
-	if (!linalg_handle)
-		return OK_SCAN_ERR( OPTKIT_ERROR_UNALLOCATED );
+	OK_CHECK_PTR(linalg_handle);
 	OK_CHECK_VECTOR(x);
 	OK_CHECK_VECTOR(y);
 	if (x->size != y->size)
@@ -63,8 +52,7 @@ ok_status blas_axpy(void * linalg_handle, ok_float alpha, const vector * x,
 ok_status blas_nrm2(void * linalg_handle, const vector *x, ok_float * result)
 {
 	ok_status err = OPTKIT_SUCCESS;
-	if (!linalg_handle)
-		return OK_SCAN_ERR( OPTKIT_ERROR_UNALLOCATED );
+	OK_CHECK_PTR(linalg_handle);
 	OK_CHECK_VECTOR(x);
 
 	err = OK_SCAN_CUBLAS( CUBLAS(nrm2)(*(cublasHandle_t *) linalg_handle,
@@ -76,8 +64,7 @@ ok_status blas_nrm2(void * linalg_handle, const vector *x, ok_float * result)
 ok_status blas_scal(void * linalg_handle, const ok_float alpha, vector *x)
 {
 	ok_status err = OPTKIT_SUCCESS;
-	if (!linalg_handle)
-		return OK_SCAN_ERR( OPTKIT_ERROR_UNALLOCATED );
+	OK_CHECK_PTR(linalg_handle);
 	OK_CHECK_VECTOR(x);
 
 	err = OK_SCAN_CUBLAS( CUBLAS(scal)(*(cublasHandle_t *) linalg_handle,
@@ -89,8 +76,7 @@ ok_status blas_scal(void * linalg_handle, const ok_float alpha, vector *x)
 ok_status blas_asum(void * linalg_handle, const vector * x, ok_float * result)
 {
 	ok_status err = OPTKIT_SUCCESS;
-	if (!linalg_handle)
-		return OK_SCAN_ERR( OPTKIT_ERROR_UNALLOCATED );
+	OK_CHECK_PTR(linalg_handle);
 	OK_CHECK_VECTOR(x);
 
 	err = OK_SCAN_CUBLAS( CUBLAS(asum)(*(cublasHandle_t *) linalg_handle,
@@ -99,18 +85,17 @@ ok_status blas_asum(void * linalg_handle, const vector * x, ok_float * result)
 	return err;
 }
 
-ok_float blas_dot(void * linalg_handle, const vector * x, const vector * y,
+ok_status blas_dot(void * linalg_handle, const vector * x, const vector * y,
 	ok_float * result)
 {
 	ok_status err = OPTKIT_SUCCESS;
-	if (!linalg_handle)
-		return OK_SCAN_ERR( OPTKIT_ERROR_UNALLOCATED );
+	OK_CHECK_PTR(linalg_handle);
 	OK_CHECK_VECTOR(x);
 	OK_CHECK_VECTOR(y);
 
 	err = OK_SCAN_CUBLAS( CUBLAS(dot)(*(cublasHandle_t *) linalg_handle,
 		(int) x->size, x->data, (int) x->stride, y->data,
-		(int) y->stride, &result) );
+		(int) y->stride, result) );
 	cudaDeviceSynchronize();
 	return err;
 }
@@ -126,7 +111,7 @@ ok_float blas_dot(void * linalg_handle, const vector * x, const vector * y,
 
 /* BLAS LEVEL 2 */
 
-ok_stauts blas_gemv(void * linalg_handle, enum CBLAS_TRANSPOSE transA,
+ok_status blas_gemv(void * linalg_handle, enum CBLAS_TRANSPOSE transA,
 	ok_float alpha, const matrix *A, const vector *x, ok_float beta,
 	vector *y)
 {
@@ -134,8 +119,7 @@ ok_stauts blas_gemv(void * linalg_handle, enum CBLAS_TRANSPOSE transA,
 	cublasOperation_t tA;
 	int s1, s2;
 
-	if (!linalg_handle)
-		return OK_SCAN_ERR( OPTKIT_ERROR_UNALLOCATED );
+	OK_CHECK_PTR(linalg_handle);
 	OK_CHECK_MATRIX(A);
 	OK_CHECK_VECTOR(x);
 	OK_CHECK_VECTOR(y);
@@ -160,7 +144,7 @@ ok_stauts blas_gemv(void * linalg_handle, enum CBLAS_TRANSPOSE transA,
 	return err;
 }
 
-ok_stauts blas_trsv(void * linalg_handle, enum CBLAS_UPLO uplo,
+ok_status blas_trsv(void * linalg_handle, enum CBLAS_UPLO uplo,
 	enum CBLAS_TRANSPOSE transA, enum CBLAS_DIAG Diag, const matrix *A,
 	vector *x)
 {
@@ -169,8 +153,7 @@ ok_stauts blas_trsv(void * linalg_handle, enum CBLAS_UPLO uplo,
 	cublasDiagType_t di;
 	cublasFillMode_t ul;
 
-	if (!linalg_handle)
-		return OK_SCAN_ERR( OPTKIT_ERROR_UNALLOCATED );
+	OK_CHECK_PTR(linalg_handle);
 	OK_CHECK_MATRIX(A);
 	OK_CHECK_VECTOR(x);
 	if (A->size1 != A->size2 || A->size1 != x->size)
@@ -195,7 +178,7 @@ ok_stauts blas_trsv(void * linalg_handle, enum CBLAS_UPLO uplo,
 	return err;
 }
 
-ok_stauts blas_sbmv(void * linalg_handle, enum CBLAS_ORDER order,
+ok_status blas_sbmv(void * linalg_handle, enum CBLAS_ORDER order,
 	enum CBLAS_UPLO uplo, const size_t num_superdiag, const ok_float alpha,
 	const vector * vecA, const vector * x, const ok_float beta, vector * y)
 {
@@ -203,9 +186,8 @@ ok_stauts blas_sbmv(void * linalg_handle, enum CBLAS_ORDER order,
 	cublasFillMode_t ul;
 	size_t lenA;
 
-	if (!linalg_handle)
-		return OK_SCAN_ERR( OPTKIT_ERROR_UNALLOCATED );
-	OK_CHECK_MATRIX(A);
+	OK_CHECK_PTR(linalg_handle);
+	OK_CHECK_MATRIX(vecA);
 	OK_CHECK_VECTOR(x);
 	OK_CHECK_VECTOR(y);
 
@@ -218,7 +200,7 @@ ok_stauts blas_sbmv(void * linalg_handle, enum CBLAS_ORDER order,
 	lenA = y->size;
 	if (num_superdiag > 0 && num_superdiag < y->size)
 		lenA = (lenA * (lenA + 1)) / 2 -
-			((lenA - num_superdiag)*(lenA - num_superdiag + 1)) / 2
+			((lenA - num_superdiag)*(lenA - num_superdiag + 1)) / 2;
 	if (x->size != y->size || vecA->size != lenA)
 		return OPTKIT_ERROR_DIMENSION_MISMATCH;
 
@@ -238,26 +220,25 @@ ok_stauts blas_sbmv(void * linalg_handle, enum CBLAS_ORDER order,
 	return err;
 }
 
-ok_stauts blas_diagmv(void * linalg_handle, const ok_float alpha,
+ok_status blas_diagmv(void * linalg_handle, const ok_float alpha,
 	const vector * vecA, const vector * x, const ok_float beta, vector * y)
 {
 	ok_status err = OPTKIT_SUCCESS;
-	if (!linalg_handle)
-		return OK_SCAN_ERR( OPTKIT_ERROR_UNALLOCATED );
+	OK_CHECK_PTR(linalg_handle);
 	OK_CHECK_VECTOR(vecA);
 	OK_CHECK_VECTOR(x);
 	OK_CHECK_VECTOR(y);
-	else if (vecA->size != y->size || x->size != y->size)
+	if (vecA->size != y->size || x->size != y->size)
 		return OK_SCAN_ERR( OPTKIT_ERROR_DIMENSION_MISMATCH );
 
-	err = OK_SCAN_CUBLAS( err, blas_sbmv(linalg_handle, CblasColMajor,
+	err = OK_SCAN_ERR( blas_sbmv(linalg_handle, CblasColMajor,
 		CblasLower, 0, alpha, vecA, x, beta, y) );
 	cudaDeviceSynchronize();
 	return err;
 }
 
 /* BLAS LEVEL 3 */
-ok_stauts blas_syrk(void * linalg_handle, enum CBLAS_UPLO uplo,
+ok_status blas_syrk(void * linalg_handle, enum CBLAS_UPLO uplo,
 	enum CBLAS_TRANSPOSE transA, ok_float alpha, const matrix * A,
 	ok_float beta, matrix * C)
 {
@@ -265,8 +246,11 @@ ok_stauts blas_syrk(void * linalg_handle, enum CBLAS_UPLO uplo,
 	cublasOperation_t tA;
 	cublasFillMode_t ul;
 
+	OK_CHECK_PTR(linalg_handle);
 	OK_CHECK_MATRIX(A);
 	OK_CHECK_MATRIX(C);
+	if (A->order != C->order)
+		return OK_SCAN_ERR( OPTKIT_ERROR_LAYOUT_MISMATCH );
 
 	const int k = (transA == CblasNoTrans) ?
 		      (int) A->size2 : (int) A->size1;
@@ -281,14 +265,6 @@ ok_stauts blas_syrk(void * linalg_handle, enum CBLAS_UPLO uplo,
 		     CUBLAS_FILL_MODE_UPPER : CUBLAS_FILL_MODE_LOWER;
 	}
 
-
-	if (!linalg_handle)
-		return OK_SCAN_ERR( OPTKIT_ERROR_UNALLOCATED );
-
-	if ( !__matrix_order_compat(A, C, "A", "C", "blas_syrk") )
-		return;
-
-
 	err = OK_SCAN_CUBLAS( CUBLAS(syrk)(*(cublasHandle_t *) linalg_handle,
 		ul, tA, (int) C->size2, k, &alpha, A->data, (int) A->ld, &beta,
 		C->data, (int) C->ld) );
@@ -296,7 +272,7 @@ ok_stauts blas_syrk(void * linalg_handle, enum CBLAS_UPLO uplo,
 	return err;
 }
 
-ok_stauts blas_gemm(void * linalg_handle, enum CBLAS_TRANSPOSE transA,
+ok_status blas_gemm(void * linalg_handle, enum CBLAS_TRANSPOSE transA,
 	enum CBLAS_TRANSPOSE transB, ok_float alpha, const matrix * A,
 	const matrix * B, ok_float beta, matrix * C)
 {
@@ -304,9 +280,12 @@ ok_stauts blas_gemm(void * linalg_handle, enum CBLAS_TRANSPOSE transA,
 	cublasOperation_t tA, tB;
 	int s1, s2;
 
+	OK_CHECK_PTR(linalg_handle);
 	OK_CHECK_MATRIX(A);
 	OK_CHECK_MATRIX(B);
 	OK_CHECK_MATRIX(C);
+	if (A->order != C->order || B->order != C->order)
+		return OK_SCAN_ERR( OPTKIT_ERROR_LAYOUT_MISMATCH );
 
 	const int k = (transA == CblasNoTrans) ?
 		      (int) A->size2 : (int) A->size1;
@@ -321,13 +300,6 @@ ok_stauts blas_gemm(void * linalg_handle, enum CBLAS_TRANSPOSE transA,
 		tB = transA == CblasTrans ? CUBLAS_OP_T : CUBLAS_OP_N;
 	}
 
-	if (!linalg_handle)
-		return OK_SCAN_ERR( OPTKIT_ERROR_UNALLOCATED );
-
-	if (!__matrix_order_compat(A, B, "A", "B", "blas_gemm") ||
-		!__matrix_order_compat(A, C, "A", "C", "blas_gemm"))
-		return;
-
 	err = OK_SCAN_CUBLAS( CUBLAS(gemm)(*(cublasHandle_t *) linalg_handle,
 		tA, tB, s1, s2, k, &alpha, A->data, (int) A->ld, B->data,
 		(int) B->ld, &beta, C->data, (int) C->ld) );
@@ -335,14 +307,13 @@ ok_stauts blas_gemm(void * linalg_handle, enum CBLAS_TRANSPOSE transA,
 	return err;
 }
 
-ok_stauts blas_trsm(void * linalg_handle, enum CBLAS_SIDE Side,
+ok_status blas_trsm(void * linalg_handle, enum CBLAS_SIDE Side,
 	enum CBLAS_UPLO uplo, enum CBLAS_TRANSPOSE transA, enum CBLAS_DIAG Diag,
 	ok_float alpha, const matrix *A, matrix *B)
 {
 	printf("\nMethod `blas_trsm()` not implemented for GPU\n");
 	return OPTKIT_ERROR;
 }
-
 
 #ifdef __cplusplus
 }
