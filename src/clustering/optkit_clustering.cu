f5f5d8d7cca283160dc8e4cf5ebe5fc4342f28c4
bungun <ungun@stanford.edu>
2016-05-12 22:13:40 -0700

    (cluster) mirror dense.cu's reduction kernels

--- src/clustering/optkit_clustering.cu
+++ src/clustering/optkit_clustering.cu
@@ -17,12 +17,13 @@ static __global__ void __assign_clusters_l2(size_t * a2c_curr,
 	const size_t stride_tentative, size_t * reassigned, const size_t i)
 {
 	size_t * new_, * curr;
-	size_t row = i + threadIdx.x;
-	if (row < len_a2c) {
+	size_t row = i + blockIdx.x * blockDim.x + threadIdx.x;
+	while (row < len_a2c) {
 		new_ = a2c_tentative + row * stride_tentative;
 		curr = a2c_curr + row * stride_curr;
 		*reassigned += (size_t)(*curr != *new_);
 		*curr = *new_;
+		row += blockDim.x * gridDim.x;
 	}
 }
 
@@ -31,7 +32,7 @@ static ok_status assign_clusters_l2(matrix * A, matrix * C,
 {
 	ok_status err = OPTKIT_SUCCESS;
 	size_t i, * reassigned;
-
+	uint grid_dim;
 
 	OK_CHECK_MATRIX(A);
 	OK_CHECK_MATRIX(C);
@@ -41,16 +42,18 @@ static ok_status assign_clusters_l2(matrix * A, matrix * C,
 		A->size2 != C->size2)
 		return OK_SCAN_ERR( OPTKIT_ERROR_DIMENSION_MISMATCH );
 
+	grid_dim = calc_grid_dim(A->size1);
 	h->reassigned = 0;
 	ok_alloc_gpu(reassigned, sizeof(size_t));
 	ok_memcpy_gpu(reassigned, &h->reassigned, sizeof(size_t));
 
-	for (i = 0; i < A->size1; i += kBlockSize) {
+	for (i = 0; i < A->size1; i += grid_dim * kBlockSize) {
 		cudaStream_t s;
 		cudaStreamCreate(&s);
-		__assign_clusters_l2<<<1, kBlockSize, 0, s>>>(a2c->indices,
-			a2c->size1, a2c->stride, h->a2c_tentative.indices,
-			h->a2c_tentative.stride, reassigned, i);
+		__assign_clusters_l2<<<grid_dim, kBlockSize, 0, s>>>(
+			a2c->indices, a2c->size1, a2c->stride,
+			h->a2c_tentative.indices, h->a2c_tentative.stride,
+			reassigned, i);
 		cudaStreamDestroy(s);
 	}
 	cudaDeviceSynchronize();
@@ -60,66 +63,69 @@ static ok_status assign_clusters_l2(matrix * A, matrix * C,
 	return err;
 }
 
-static __global__ void __dist_lInf_A_minus_UC(const ok_float * A,
-	const size_t rowsA, const size_t colsA,
-	const size_t row_stride_A, const size_t col_stride_A,
-	const size_t * a2c, const size_t stride_a2c, const ok_float * C,
-	const size_t row_stride_C, const size_t col_stride_C,
-	ok_float * distances, const size_t stride_dist,
-	const size_t i, const size_t j)
+namespace optkit {
+static __device__ ok_float reduce_amuc_max(ok_float & val, const ok_float & a,
+	const ok_float & c, const ok_float & maxdist)
 {
-	uint row = threadIdx.x;
-	uint col = threadIdx.y;
-	uint global_row = i + row;
-	uint global_col = j + col;
-	const uint kTileLD = kTileSize + 1u;
-	__shared__ ok_float Asub[kTileSize * kTileLD];
-	__shared__ ok_float Csub[kTileSize * kTileLD];
-	__shared__ ok_float dist_sub[kTileSize];
-
-	if (global_row >= rowsA || global_col >= colsA)
-		return;
-
-	/* initialize distances -> 0 */
-	if (col == 0)
-		dist_sub[row] = 0;
-	__syncthreads();
-
-	/* copy Asub = A */
-	Asub[row * kTileLD + col] = A[global_row * row_stride_A +
-		global_col * col_stride_A];
-
-	/* Csub = UC */
-	Csub[row * kTileLD + col] = C[a2c[global_row * stride_a2c] *
-		row_stride_A + global_col * col_stride_A];
-	__syncthreads();
-
-	/* A -= UC */
-	Asub[row * kTileLD + col] -= Csub[row * kTileLD + col];
-	__syncthreads();
-
-	/* set amax */
-	dist_sub[row] = MATH(fmax)(dist_sub[row],
-		MATH(fabs)(Asub[row * kTileLD + col]));
-	__syncthreads();
-
-	/* copy distances */
-	if (col == 0)
-		distances[global_row * stride_dist] = MATH(fmax)(
-			distances[global_row * stride_dist], dist_sub[row]);
+	val = MATH(fmax)(val, MATH(fabs)(a - c));
+	return (ok_float) val > maxdist;
 }
 
-static __global__ void __assign_clusters_l2_lInf_cap(size_t * a2c_curr,
-	const size_t len_a2c, const size_t stride_curr, size_t * a2c_tentative,
-	const size_t stride_tentative, const ok_float * distances,
-	const size_t stride_dist, const ok_float maxdist, size_t * reassigned,
-	const size_t i)
+static __device__ ok_float reduce_max(ok_float & first, const ok_float & second,
+	const ok_float & maxdist)
 {
-	uint row = i + threadIdx.x;
-	if (row < len_a2c && distances[row * stride_dist] <= maxdist) {
-		a2c_curr[row * stride_curr] =
-			a2c_tentative[row * stride_tentative];
-		++(*reassigned);
+	first = MATH(fmax)(first, second);
+	return (ok_float) first > maxdist;
+}
+
+
+}
+
+template<uint blockSize>
+static __global__ void __dist_lInf_a_minus_uc(ok_float * const a,
+	const size_t stride_a, size_t * const u, const size_t stride_u,
+	ok_float * const C, const size_t rowstride_c, const size_t colstride_c,
+	const size_t ncols, const ok_float maxdist, ok_float * max_violated);
+{
+	ok_float * c = C + u[stride_u] * rowstride_c;
+	uint col = threadIdx.x;
+	uint block_stride = 2 * blockSize;
+	uint global_col = blockIdx.x * block_stride + col;
+	uint gride_stride = gridDim.x * block_stride;
+	__shared__ ok_float a_minus_c[blockSize];
+
+	/* copy Asub = A */
+	while (global_col < ncols && max_violated == 0) {
+		*max_violated += optkit::reduce_amuc_max(a_minus_c[col],
+			a[global_col * stride_a],
+			c[global_col * stride_c]);
+		if (global_col + blockSize < ncols && max_violated == 0)
+			*max_violated += optkit::reduce_amuc_max(a_minus_c[col],
+				a[(global_col + blockSize) * stride_a],
+				c[(global_col + blockSize) * stride_c]);
+		global_col += gride_stride;
+	}
+	__syncthreads();
+
+
+  }
+
+static __global__ void __assign_clusters_l2_lInf_cap(size_t * a2c_curr,
+	const size_t len_a2c, const size_t stride_curr,
+	size_t * const a2c_tentative, const size_t stride_tentative,
+	ok_float * const dist_violations, const size_t stride_dv,
+	size_t * reassigned, const size_t i)
+{
+	size_t * new_, * curr;
+	size_t row = i + blockIdx.x * blockDim.x + threadIdx.x;
+	while (row < len_a2c) {
+		new_ = a2c_tentative + row * stride_tentative;
+		curr = a2c_curr + row * stride_curr;
+		if (dist_violations[row * stride_dv] == 0) {
+			*reassigned += (size_t)(*curr != *new_);
+			*curr = *new_;
+		}
+		row += blockDim.x * gridDim.x;
 	}
 }
 
@@ -138,11 +144,7 @@ static ok_status assign_clusters_l2_lInf_cap(matrix * A, matrix * C,
 	upsamplingvec * a2c, cluster_aid * h, ok_float maxdist)
 {
 	ok_status err = OPTKIT_SUCCESS;
-	uint i, j;
-	uint block_size = kTiles2D * kTileSize;
-
-	dim3 grid_dim(kTileSize, kTileSize);
-	dim3 blk_dim(kTiles2D, kTiles2D);
+	uint i, grid_dim;
 
 	OK_CHECK_MATRIX(A);
 	OK_CHECK_MATRIX(C);
@@ -156,7 +158,6 @@ static ok_status assign_clusters_l2_lInf_cap(matrix * A, matrix * C,
 	size_t col_stride_A = (A->order == CblasRowMajor) ? 1 : A->ld;
 	size_t row_stride_C = (C->order == CblasRowMajor) ? C->ld : 1;
 	size_t col_stride_C = (C->order == CblasRowMajor) ? 1 : A->ld;
-
 	size_t * reassigned;
 
 	vector * dmax = &h->d_min;
@@ -168,25 +169,31 @@ static ok_status assign_clusters_l2_lInf_cap(matrix * A, matrix * C,
 	/* zero out distances */
 	vector_scale(dmax, kZero);
 
-	/* reduce one bundle of [block_size] rows x N cols per stream */
-	for (i = 0; i < A->size1; i += block_size) {
+	for (i = 0; i < A->size1; ++i) {
 		cudaStream_t s;
 		cudaStreamCreate(&s);
-		for (j = 0; j < A->size2; j += block_size)
-			__dist_lInf_A_minus_UC<<<grid_dim, blk_dim, 0, s>>>(
-				A->data, A->size1, A->size2, row_stride_A,
-				col_stride_A, a2c->indices, a2c->stride,
-				C->data, row_stride_C, col_stride_C,
-				dmax->data, dmax->stride, i, j);
+		__dist_lInf_a_minus_uc<kBlockSize>
+		<<<grid_dim, kBlockSize, 0, s>>>(A->data + i * row_stride_A,
+			col_stride_A, a2c->indices, a2c->stride, C->data,
+			row_stride_C, col_stride_C, A->size2, maxdist,
+			dmax->data + i * dmax->stride);
+		cudaStreamDestroy(s);
+	}
+	cudaDeviceSynchronize();
+	err = OK_STATUS_CUDA;
 
-		__assign_clusters_l2_lInf_cap<<<1, block_size, 0, s>>>(
+	grid_dim = calc_grid_dim(A->size1);
+	for (i = 0; i < A->size1 && !err; i += grid_dim * kBlockSize) {
+		cudaStream_t s;
+		cudaStreamCreate(&s);
+		__assign_clusters_l2_lInf_cap<<<grid_dim, kBlockSize, 0, s>>>(
 			a2c->indices, a2c->size1, a2c->stride,
 			h->a2c_tentative.indices, h->a2c_tentative.stride,
 			dmax->data, dmax->stride, maxdist, reassigned, i);
 		cudaStreamDestroy(s);
 	}
 	cudaDeviceSynchronize();
-	CUDA_CHECK_ERR;
+	OK_STATUS_CUDA;
 
 	ok_memcpy_gpu(&h->reassigned, reassigned, sizeof(size_t));
 	return err;
