a9b83e70d57339906169673cc3945a0b8ba7a43f
bungun <ungun@stanford.edu>
2017-11-26 03:59:47 -0800

    change tabs to spaces

--- python/optkit/tests/defs.py
+++ python/optkit/tests/defs.py
@@ -14,73 +14,73 @@ DEFAULT_ROWS = os.getenv('OPTKIT_TESTING_DEFAULT_NROWS', None)
 DEFAULT_COLS = os.getenv('OPTKIT_TESTING_DEFAULT_NCOLS', None)
 DEFAULT_MATRIX_PATH = os.getenv('OPTKIT_TESTING_DEFAULT_MATRIX', None)
 DEFAULT_SPARSE_OCCUPANCY = os.getenv('OPTKIT_TESTING_DEFAULT_SPARSE_OCCUPANCY',
-								  0.4)
+                                  0.4)
 
 if DEFAULT_MATRIX_PATH is not None:
-	if not os.path.exsists(DEFAULT_MATRIX_PATH):
-		DEFAULT_MATRIX_PATH = None
-	elif not DEFAULT_MATRIX_PATH.endswith('.npy'):
-		DEFAULT_MATRIX_PATH = None
+    if not os.path.exsists(DEFAULT_MATRIX_PATH):
+        DEFAULT_MATRIX_PATH = None
+    elif not DEFAULT_MATRIX_PATH.endswith('.npy'):
+        DEFAULT_MATRIX_PATH = None
 
 DEFAULT_MATRIX = None
 if DEFAULT_MATRIX_PATH is not None:
-	try:
-		DEFAULT_MATRIX = np.load(DEFAULT_MATRIX_PATH)
-	except:
-		pass
+    try:
+        DEFAULT_MATRIX = np.load(DEFAULT_MATRIX_PATH)
+    except:
+        pass
 
 if DEFAULT_ROWS is not None:
-	try:
-		DEFAULT_ROWS = int(DEFAULT_ROWS)
-	except:
-		DEFAULT_ROWS = None
+    try:
+        DEFAULT_ROWS = int(DEFAULT_ROWS)
+    except:
+        DEFAULT_ROWS = None
 if DEFAULT_COLS is not None:
-	try:
-		DEFAULT_COLS = int(DEFAULT_COLS)
-	except:
-		DEFAULT_COLS = None
+    try:
+        DEFAULT_COLS = int(DEFAULT_COLS)
+    except:
+        DEFAULT_COLS = None
 
 if DEFAULT_ROWS is not None:
-	DEFAULT_SHAPE = (DEFAULT_ROWS, DEFAULT_SHAPE[1])
+    DEFAULT_SHAPE = (DEFAULT_ROWS, DEFAULT_SHAPE[1])
 if DEFAULT_COLS is not None:
-	DEFAULT_SHAPE = (DEFAULT_SHAPE[0], DEFAULT_COLS)
+    DEFAULT_SHAPE = (DEFAULT_SHAPE[0], DEFAULT_COLS)
 
 class OptkitTestCase(unittest.TestCase):
-	VERBOSE_TEST = os.getenv('OPTKIT_TEST_VERBOSE', False)
+    VERBOSE_TEST = os.getenv('OPTKIT_TEST_VERBOSE', False)
 
-	# library conditions: gpu = True/False, single_precision = True/False
-	CONDITIONS = [(a, b) for a in (True, False) for b in (True, False)]
+    # library conditions: gpu = True/False, single_precision = True/False
+    CONDITIONS = [(a, b) for a in (True, False) for b in (True, False)]
 
-	__nnz = 0
+    __nnz = 0
 
-	@property
-	def nnz(self):
-	    return self.__nnz
+    @property
+    def nnz(self):
+        return self.__nnz
 
-	@property
-	def shape(self):
-		if DEFAULT_MATRIX is not None:
-			return DEFAULT_MATRIX.shape
-		else:
-			return DEFAULT_SHAPE
+    @property
+    def shape(self):
+        if DEFAULT_MATRIX is not None:
+            return DEFAULT_MATRIX.shape
+        else:
+            return DEFAULT_SHAPE
 
-	@property
-	def A_test_gen(self):
-		if DEFAULT_MATRIX is not None:
-			return DEFAULT_MATRIX
-		else:
-			return np.random.rand(*self.shape)
+    @property
+    def A_test_gen(self):
+        if DEFAULT_MATRIX is not None:
+            return DEFAULT_MATRIX
+        else:
+            return np.random.rand(*self.shape)
 
-	@property
-	def A_test_sparse_gen(self):
-		A_ = DEFAULT_MATRIX if DEFAULT_MATRIX else np.random.rand(*self.shape)
-		mask = np.random.rand(*self.shape) < DEFAULT_SPARSE_OCCUPANCY
-		self.__nnz = sum(sum(mask))
-		return A_ * mask
+    @property
+    def A_test_sparse_gen(self):
+        A_ = DEFAULT_MATRIX if DEFAULT_MATRIX else np.random.rand(*self.shape)
+        mask = np.random.rand(*self.shape) < DEFAULT_SPARSE_OCCUPANCY
+        self.__nnz = sum(sum(mask))
+        return A_ * mask
 
-	@staticmethod
-	def version_string(major, minor, change, status):
-		v = '{}.{}.{}'.format(major, minor, change)
-		if status:
-			v.join('-{}'.format(chr(status)))
-		return v
+    @staticmethod
+    def version_string(major, minor, change, status):
+        v = '{}.{}.{}'.format(major, minor, change)
+        if status:
+            v.join('-{}'.format(chr(status)))
+        return v
