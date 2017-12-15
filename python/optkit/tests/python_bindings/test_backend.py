c85138ab144de9d1a6ce9169a1002638c46b5d95
bungun <ungun@stanford.edu>
2017-12-15 23:05:50 +0700

    (test/bindings) tabs->spaces

--- python/optkit/tests/python_bindings/test_backend.py
+++ python/optkit/tests/python_bindings/test_backend.py
@@ -8,69 +8,69 @@ from optkit.api import backend
 from optkit import set_backend
 
 class BackendTestCase(unittest.TestCase):
-	def test_libs(self):
-		pogs_libnames = []
-		pogs_abstract_libnames = []
-		cluster_libnames = []
+    def test_libs(self):
+        pogs_libnames = []
+        pogs_abstract_libnames = []
+        cluster_libnames = []
 
-		self.assertIsNotNone( backend.pogs_lib_loader )
-		self.assertIsNotNone( backend.pogs_abstract_lib_loader )
-		self.assertIsNotNone( backend.cluster_lib_loader )
+        self.assertIsNotNone( backend.pogs_lib_loader )
+        self.assertIsNotNone( backend.pogs_abstract_lib_loader )
+        self.assertIsNotNone( backend.cluster_lib_loader )
 
-		for libkey in backend.pogs_lib_loader.libs:
-			if backend.pogs_lib_loader.libs[libkey] is not None:
-				pogs_libnames.append(libkey)
+        for libkey in backend.pogs_lib_loader.libs:
+            if backend.pogs_lib_loader.libs[libkey] is not None:
+                pogs_libnames.append(libkey)
 
-		for libkey in backend.pogs_lib_loader.libs:
-			if backend.pogs_abstract_lib_loader.libs[libkey] is not None:
-				pogs_abstract_libnames.append(libkey)
+        for libkey in backend.pogs_lib_loader.libs:
+            if backend.pogs_abstract_lib_loader.libs[libkey] is not None:
+                pogs_abstract_libnames.append(libkey)
 
-		for libkey in backend.cluster_lib_loader.libs:
-			if backend.cluster_lib_loader.libs[libkey] is not None:
-				cluster_libnames.append(libkey)
+        for libkey in backend.cluster_lib_loader.libs:
+            if backend.cluster_lib_loader.libs[libkey] is not None:
+                cluster_libnames.append(libkey)
 
-		print('available libs, POGS:', pogs_libnames)
-		self.assertTrue( len(pogs_libnames) > 0 )
+        print('available libs, POGS:', pogs_libnames)
+        self.assertTrue( len(pogs_libnames) > 0 )
 
-		print('available libs, POGS abstract:', pogs_abstract_libnames)
-		self.assertTrue( len(pogs_abstract_libnames) > 0 )
+        print('available libs, POGS abstract:', pogs_abstract_libnames)
+        self.assertTrue( len(pogs_abstract_libnames) > 0 )
 
-		print('avaialable libs, clustering:', cluster_libnames)
-		self.assertTrue( len(cluster_libnames) > 0 )
+        print('avaialable libs, clustering:', cluster_libnames)
+        self.assertTrue( len(cluster_libnames) > 0 )
 
-		configs = []
-		for libname in pogs_libnames:
-			gpu = 'gpu' in libname
-			double = '64' in libname
+        configs = []
+        for libname in pogs_libnames:
+            gpu = 'gpu' in libname
+            double = '64' in libname
 
-			self.assertEqual( backend.reset_device(), 0 )
-			self.assertEqual( set_backend(gpu=gpu, double=double), 0 )
+            self.assertEqual( backend.reset_device(), 0 )
+            self.assertEqual( set_backend(gpu=gpu, double=double), 0 )
 
-			self.assertIsNotNone( backend.pogs )
-			self.assertIsNotNone( backend.pogs_abstract )
-			self.assertIsNotNone( backend.cluster )
+            self.assertIsNotNone( backend.pogs )
+            self.assertIsNotNone( backend.pogs_abstract )
+            self.assertIsNotNone( backend.cluster )
 
-			self.assertEqual( backend.device_is_gpu, gpu )
-			self.assertNotEqual( backend.precision_is_32bit, double )
+            self.assertEqual( backend.device_is_gpu, gpu )
+            self.assertNotEqual( backend.precision_is_32bit, double )
 
-			s = optkit.api.PogsSolver(np.random.rand(10, 5))
-			self.assertIsNotNone( s )
-			self.assertFalse( backend.device_reset_allowed )
-			del s
-			self.assertTrue( backend.device_reset_allowed )
-			backend.reset_device()
+            s = optkit.api.PogsSolver(np.random.rand(10, 5))
+            self.assertIsNotNone( s )
+            self.assertFalse( backend.device_reset_allowed )
+            del s
+            self.assertTrue( backend.device_reset_allowed )
+            backend.reset_device()
 
-			s = optkit.api.PogsAbstractSolver(np.random.rand(10, 5))
-			self.assertIsNotNone( s )
-			self.assertFalse( backend.device_reset_allowed )
-			del s
-			self.assertTrue( backend.device_reset_allowed )
-			backend.reset_device()
+            s = optkit.api.PogsAbstractSolver(np.random.rand(10, 5))
+            self.assertIsNotNone( s )
+            self.assertFalse( backend.device_reset_allowed )
+            del s
+            self.assertTrue( backend.device_reset_allowed )
+            backend.reset_device()
 
-			c = optkit.api.Clustering()
-			self.assertIsNotNone( c )
-			c.kmeans_work_init(10, 5, 5)
-			self.assertFalse( backend.device_reset_allowed )
-			del c
-			self.assertTrue( backend.device_reset_allowed )
-			backend.reset_device()
+            c = optkit.api.Clustering()
+            self.assertIsNotNone( c )
+            c.kmeans_work_init(10, 5, 5)
+            self.assertFalse( backend.device_reset_allowed )
+            del c
+            self.assertTrue( backend.device_reset_allowed )
+            backend.reset_device()
--- python/optkit/tests/python_bindings/test_pogs_bindings.py
+++ python/optkit/tests/python_bindings/test_pogs_bindings.py
@@ -11,166 +11,166 @@ from optkit.tests.defs import OptkitTestCase, TEST_ITERATE
 print('BACKEND:', backend.config)
 
 class PogsBindingsTestCase(OptkitTestCase):
-	@classmethod
-	def setUpClass(self):
-		self.env_orig = os.getenv('OPTKIT_USE_LOCALLIBS', '0')
-		os.environ['OPTKIT_USE_LOCALLIBS'] = '1'
-		self.__files = []
+    @classmethod
+    def setUpClass(self):
+        self.env_orig = os.getenv('OPTKIT_USE_LOCALLIBS', '0')
+        os.environ['OPTKIT_USE_LOCALLIBS'] = '1'
+        self.__files = []
 
-	@classmethod
-	def tearDownClass(self):
-		os.environ['OPTKIT_USE_LOCALLIBS'] = self.env_orig
-		for f in self.__files:
-			if os.path.exists(f):
-				os.remove(f)
+    @classmethod
+    def tearDownClass(self):
+        os.environ['OPTKIT_USE_LOCALLIBS'] = self.env_orig
+        for f in self.__files:
+            if os.path.exists(f):
+                os.remove(f)
 
-	def setUp(self):
-		self.A_test = self.A_test_gen
-		self.A_test_csr = sp.csr_matrix(self.A_test_sparse_gen)
-		self.A_test_csc = sp.csc_matrix(self.A_test_sparse_gen)
-		self.test_matrices = [self.A_test, self.A_test_csc, self.A_test_csr]
+    def setUp(self):
+        self.A_test = self.A_test_gen
+        self.A_test_csr = sp.csr_matrix(self.A_test_sparse_gen)
+        self.A_test_csc = sp.csc_matrix(self.A_test_sparse_gen)
+        self.test_matrices = [self.A_test, self.A_test_csc, self.A_test_csr]
 
-	def register_file(self, file):
-		if os.path.exists(file):
-			self.__files.append(file)
+    def register_file(self, file):
+        if os.path.exists(file):
+            self.__files.append(file)
 
-	def test_objective(self):
-		m = self.shape[0]
-		f = PogsObjective(m, h='Abs', b=1, d=2, e=3)
-		h, a, b, c, d, e, s = f.arrays
+    def test_objective(self):
+        m = self.shape[0]
+        f = PogsObjective(m, h='Abs', b=1, d=2, e=3)
+        h, a, b, c, d, e, s = f.arrays
 
-		for i in xrange(m):
-			self.assertEqual(h[i], f.enums.dict['Abs'])
-			self.assertAlmostEqual(a[i], 1)
-			self.assertAlmostEqual(b[i], 1)
-			self.assertAlmostEqual(c[i], 1)
-			self.assertAlmostEqual(d[i], 2)
-			self.assertAlmostEqual(e[i], 3)
-			self.assertAlmostEqual(s[i], 1)
+        for i in xrange(m):
+            self.assertEqual(h[i], f.enums.dict['Abs'])
+            self.assertAlmostEqual(a[i], 1)
+            self.assertAlmostEqual(b[i], 1)
+            self.assertAlmostEqual(c[i], 1)
+            self.assertAlmostEqual(d[i], 2)
+            self.assertAlmostEqual(e[i], 3)
+            self.assertAlmostEqual(s[i], 1)
 
-		# set block by scalar
-		f.set(end=int(m / 2), b=0.5)
-		for i in xrange(int(m / 2)):
-			self.assertEqual(f.b[i], 0.5)
+        # set block by scalar
+        f.set(end=int(m / 2), b=0.5)
+        for i in xrange(int(m / 2)):
+            self.assertEqual(f.b[i], 0.5)
 
-		# set block by vector
-		c = np.random.rand(int(m / 4))
-		f.set(start=int(m / 2), end=int(m / 2) + int(m / 4), c=c)
-		for i, idx in enumerate(
-				xrange(int(m / 2), int(m / 2) + int(m / 4))):
-			self.assertAlmostEqual(f.c[idx], c[i])
+        # set block by vector
+        c = np.random.rand(int(m / 4))
+        f.set(start=int(m / 2), end=int(m / 2) + int(m / 4), c=c)
+        for i, idx in enumerate(
+                xrange(int(m / 2), int(m / 2) + int(m / 4))):
+            self.assertAlmostEqual(f.c[idx], c[i])
 
-		# set indices by scalar
-		indices = []
-		threshold = lambda i: i if np.random.rand() > 0.75 else False
-		while len(indices) == 0:
-			indices = listfilter(None, listmap(threshold, xrange(m)))
-		f.set(range=indices, e=0.1)
-		for idx in indices:
-			self.assertAlmostEqual(f.e[idx], 0.1)
+        # set indices by scalar
+        indices = []
+        threshold = lambda i: i if np.random.rand() > 0.75 else False
+        while len(indices) == 0:
+            indices = listfilter(None, listmap(threshold, xrange(m)))
+        f.set(range=indices, e=0.1)
+        for idx in indices:
+            self.assertAlmostEqual(f.e[idx], 0.1)
 
-		# set indices by vector
-		d = np.random.rand(len(indices))
-		f.set(range=indices, d=d)
-		for i, idx in enumerate(indices):
-			self.assertAlmostEqual(f.d[idx], d[i])
+        # set indices by vector
+        d = np.random.rand(len(indices))
+        f.set(range=indices, d=d)
+        for i, idx in enumerate(indices):
+            self.assertAlmostEqual(f.d[idx], d[i])
 
-		# block copy
-		msub = int(m / 3)
-		fsub = PogsObjective(
-				msub, b=np.random.rand(msub), d=np.random.rand(msub))
-		f.copy_from(fsub)
-		self.assertEqual( sum(f.b[:msub] - fsub.b), 0 )
-		self.assertEqual( sum(f.d[:msub] - fsub.d), 0 )
+        # block copy
+        msub = int(m / 3)
+        fsub = PogsObjective(
+                msub, b=np.random.rand(msub), d=np.random.rand(msub))
+        f.copy_from(fsub)
+        self.assertEqual( sum(f.b[:msub] - fsub.b), 0 )
+        self.assertEqual( sum(f.d[:msub] - fsub.d), 0 )
 
-		start_idx = int(m / 2)
-		f.copy_from(fsub, start_idx)
-		self.assertEqual( sum(f.b[start_idx:start_idx + msub] - fsub.b), 0 )
-		self.assertEqual( sum(f.d[start_idx:start_idx + msub] - fsub.d), 0 )
+        start_idx = int(m / 2)
+        f.copy_from(fsub, start_idx)
+        self.assertEqual( sum(f.b[start_idx:start_idx + msub] - fsub.b), 0 )
+        self.assertEqual( sum(f.d[start_idx:start_idx + msub] - fsub.d), 0 )
 
-	def test_solver_object(self):
-		s = PogsSolver(self.A_test)
-		print(type(s))
-		self.assertFalse( backend.device_reset_allowed )
-		del s
-		gc.collect()
-		self.assertTrue( backend.device_reset_allowed )
+    def test_solver_object(self):
+        s = PogsSolver(self.A_test)
+        print(type(s))
+        self.assertFalse( backend.device_reset_allowed )
+        del s
+        gc.collect()
+        self.assertTrue( backend.device_reset_allowed )
 
-	# def test_abstract_solver_object(self):
-	# 	for A in [self.A_test_csc, self.A_test_csr]:
-	# 		s = PogsAbstractSolver(A)
-	# 		self.assertFalse( backend.device_reset_allowed )
-	# 		del s
-	# 		gc.collect()
-	# 		self.assertTrue( backend.device_reset_allowed )
+    # def test_abstract_solver_object(self):
+    #   for A in [self.A_test_csc, self.A_test_csr]:
+    #       s = PogsAbstractSolver(A)
+    #       self.assertFalse( backend.device_reset_allowed )
+    #       del s
+    #       gc.collect()
+    #       self.assertTrue( backend.device_reset_allowed )
 
-	def exercise_solve_call(self, solver_constructor, test_matrix):
-		# objectives = ('Abs', 'AbsQuad', 'AbsExp', 'AsymmSquare')
-		objectives = ['Abs']
-		for h in objectives:
-			if self.VERBOSE_TEST:
-				print('objective:', h)
-			asymm = 2. if h != 'Abs' else 1.
-			s = solver_constructor(test_matrix)
-			f = PogsObjective(self.shape[0], h=h, b=1, s=asymm)
-			g = PogsObjective(self.shape[1], h='IndGe0')
-			s.solve(f, g)
-			self.assertEqual(s.info.err, 0)
-			self.assertTrue(
-					s.info.converged or s.info.iters == s.settings.maxiter)
-			del s
+    def exercise_solve_call(self, solver_constructor, test_matrix):
+        # objectives = ('Abs', 'AbsQuad', 'AbsExp', 'AsymmSquare')
+        objectives = ['Abs']
+        for h in objectives:
+            if self.VERBOSE_TEST:
+                print('objective:', h)
+            asymm = 2. if h != 'Abs' else 1.
+            s = solver_constructor(test_matrix)
+            f = PogsObjective(self.shape[0], h=h, b=1, s=asymm)
+            g = PogsObjective(self.shape[1], h='IndGe0')
+            s.solve(f, g)
+            self.assertEqual(s.info.err, 0)
+            self.assertTrue(
+                    s.info.converged or s.info.iters == s.settings.maxiter)
+            del s
 
-	def test_solve_call(self):
-		print(backend.config)
-		self.exercise_solve_call(PogsSolver, self.A_test)
+    def test_solve_call(self):
+        print(backend.config)
+        self.exercise_solve_call(PogsSolver, self.A_test)
 
-	# def test_abstract_solve_call(self):
-	# 	n = 1
-	# 	for test_matrix in self.test_matrices:
-	# 		print('abstract solve: {}/{}---{}'.format(
-	# 				n, len(self.test_matrices), ))
-	# 		n += 1
-	# 		self.exercise_solve_call(PogsAbstractSolver, test_matrix)
+    # def test_abstract_solve_call(self):
+    #   n = 1
+    #   for test_matrix in self.test_matrices:
+    #       print('abstract solve: {}/{}---{}'.format(
+    #               n, len(self.test_matrices), ))
+    #       n += 1
+    #       self.exercise_solve_call(PogsAbstractSolver, test_matrix)
 
-	def test_solver_io(self):
-		f = PogsObjective(self.shape[0], h='Abs', b=1)
-		g = PogsObjective(self.shape[1], h='IndGe0')
+    def test_solver_io(self):
+        f = PogsObjective(self.shape[0], h='Abs', b=1)
+        g = PogsObjective(self.shape[1], h='IndGe0')
 
-		s = PogsSolver(self.A_test)
+        s = PogsSolver(self.A_test)
 
-		cache_file = s.save(os.path.abspath('.'), 'c_solve_test')
-		self.register_file(cache_file)
+        cache_file = s.save(os.path.abspath('.'), 'c_solve_test')
+        self.register_file(cache_file)
 
-		s.solve(f, g, resume=0, maxiter=10000)
+        s.solve(f, g, resume=0, maxiter=10000)
 
-		s2 = PogsSolver(self.A_test, 'no_init')
-		s2.load(os.path.abspath('.'), 'c_solve_test')
-		s2.solve(f, g, resume=0, maxiter=10000)
+        s2 = PogsSolver(self.A_test, 'no_init')
+        s2.load(os.path.abspath('.'), 'c_solve_test')
+        s2.solve(f, g, resume=0, maxiter=10000)
 
-		cache_file = s2.save(os.path.abspath('.'),'c_solve_test2')
-		self.register_file(cache_file)
+        cache_file = s2.save(os.path.abspath('.'),'c_solve_test2')
+        self.register_file(cache_file)
 
-		s3 = PogsSolver(self.A_test, 'no_init')
-		s3.load(os.path.abspath('.'), 'c_solve_test2')
-		s3.solve(f, g, resume=1, maxiter=10000)
+        s3 = PogsSolver(self.A_test, 'no_init')
+        s3.load(os.path.abspath('.'), 'c_solve_test2')
+        s3.solve(f, g, resume=1, maxiter=10000)
 
-		factor = 30. if backend.pogs.pyfloat == np.float32 else 10.
-		factor *= 2.**TEST_ITERATE
-		self.assertTrue(s3.info.c.k <= s2.info.c.k or not s3.info.c.converged)
+        factor = 30. if backend.pogs.pyfloat == np.float32 else 10.
+        factor *= 2.**TEST_ITERATE
+        self.assertTrue(s3.info.c.k <= s2.info.c.k or not s3.info.c.converged)
 
-		diff_12 = abs(s2.info.c.obj - s.info.c.obj)
-		diff_13 = abs(s3.info.c.obj - s.info.c.obj)
+        diff_12 = abs(s2.info.c.obj - s.info.c.obj)
+        diff_13 = abs(s3.info.c.obj - s.info.c.obj)
 
-		tolerance = factor * s.settings.c.reltol * max(1, abs(s.info.objval))
+        tolerance = factor * s.settings.c.reltol * max(1, abs(s.info.objval))
 
-		self.assertTrue(diff_12 <= tolerance or
-						not s2.info.converged or
-						not s.info.converged)
+        self.assertTrue(diff_12 <= tolerance or
+                        not s2.info.converged or
+                        not s.info.converged)
 
-		self.assertTrue(diff_13 <= tolerance or
-						not s3.info.converged or
-						not s.info.converged)
+        self.assertTrue(diff_13 <= tolerance or
+                        not s3.info.converged or
+                        not s.info.converged)
 
-		del s
-		del s2
-		del s3
+        del s
+        del s2
+        del s3
