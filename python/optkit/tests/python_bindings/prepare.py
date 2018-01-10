b4eb075439911b22d20c8ecc063c6cada9e29d15
bungun <ungun@stanford.edu>
2018-01-09 17:56:51 -0800

    (test) correct mechanism for checking pytest device env var

--- python/optkit/tests/python_bindings/prepare.py
+++ python/optkit/tests/python_bindings/prepare.py
@@ -5,5 +5,5 @@ import os
 from optkit import api
 
 def establish_backend():
-    if os.getenv('OPTKIT_PYTEST_GPU', False):
+    if int(os.getenv('OPTKIT_PYTEST_GPU', 0)):
         api.set_backend(gpu=True)
